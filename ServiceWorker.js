27a008bf560260cf365d8a37f5e78b6033273db7
anhhuynhnhut <huynhnhutanh2000@gmail.com>
2024-07-19 19:41:17 +0700

    đá

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "VGU-VT-Official-0.1";
-const contentToCache = [
-    "Build/2d-3d.loader.js",
-    "Build/2d-3d.framework.js",
-    "Build/2d-3d.data",
-    "Build/2d-3d.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
